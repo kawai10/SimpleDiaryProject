60656e06dcc62f6cab05d4a150d27ca1db20caab
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-11-08 16:53:21 +0900

    프로젝트 기초 세팅 1
    header와 버튼 만들기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,28 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import RouteTest from './components/RouteTest';
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<div className="App">
+				<MyHeader
+					headText={'App'}
+					leftChild={<MyButton text={'왼쪽 버튼'} />}
+					rightChild={<MyButton text={'오른쪽 버튼'} />}
+				/>
 				<h1>App</h1>
+				<MyButton text={'버튼'} type={'positive'} />
+				<MyButton text={'버튼'} type={'negative'} />
+				<MyButton text={'버튼'} />
 				<Routes>
 					<Route path={'/'} element={<Home />} />
 					<Route path={'/new'} element={<New />} />
 					<Route path={'/edit'} element={<Edit />} />
 					<Route path={'diary/:id'} element={<Diary />} />
 				</Routes>
-				<RouteTest />
 			</div>
 		</BrowserRouter>
 	);
