b843dff82b85cc3081eb332047c74bc1da864691
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-11-08 16:08:52 +0900

    페이지 라우팅 - React Router 응용
    useParams, searchParams, useNavigate

--- frontend/src/pages/Edit.jsx
+++ frontend/src/pages/Edit.jsx
@@ -1,7 +1,26 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
 const Edit = () => {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const navigate = useNavigate();
+
+	const id = searchParams.get('id');
+	const mode = searchParams.get('mode');
+
 	return (
 		<div>
 			<h1>Edit</h1>
+			<button
+				onClick={() =>
+					setSearchParams({
+						who: 'winter',
+					})
+				}>
+				QS 바꾸기
+			</button>
+			<button onClick={() => navigate('/')}>Home 가기</button>
+			<button onClick={() => navigate(-1)}>뒤로가기</button>
 		</div>
 	);
 };
